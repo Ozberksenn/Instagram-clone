59e24d22fc9dcb6b568b1b2f4ad519f15b6915e2
Özberk Şen <96706849+Ozberksenn@users.noreply.github.com>
2022-08-20 03:21:19 +0300

    last

--- src/Components/ContentList/ContentList.js
+++ src/Components/ContentList/ContentList.js
@@ -3,7 +3,6 @@ import {View, FlatList} from 'react-native';
 import data from '../../data.json';
 import Content from '../Content/Content';
 function ContentList() {
-  console.log(data);
   return (
     <View>
       <FlatList
